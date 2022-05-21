c3bf08cd7bb12979aedc7c9021d67ea9be350513
saedaraed <73895774+saedaraed@users.noreply.github.com>
2022-05-21 20:02:55 +0300

    Update index.js

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,69 +1,75 @@
-import "./style.css";
-import InputBox from "../../components/InputBox";
-import List from "../../components/List";
-import { Component } from "react";
+import React, { useState } from 'react';
+import InputBox from '../../components/InputBox';
+import List from '../../components/List';
+import './style.css';
 
-class Home extends Component {
-    state = {
-        value: "",
-        items: [{
-                title: "Yazn",
-                id: Math.random() * 1000,
-            },
-            {
-                title: "Yazn",
-                id: Math.random() * 1000,
-            },
-            {
-                title: "Moath",
-                id: Math.random() * 1000,
-            },
-            {
-                title: "Husam",
-                id: Math.random() * 1000,
-            },
-        ],
-    };
+export default function Index  () {
+  const [value, setValue] = useState('');
+  cont[(listItems, setListItems)] = useState([
+    {
+      title: 'Yazn',
+      id: Math.random() * 1000,
+    },
+    {
+      title: 'Yazn',
+      id: Math.random() * 1000,
+    },
+    {
+      title: 'Moath',
+      id: Math.random() * 1000,
+    },
+    {
+      title: 'Husam',
+      id: Math.random() * 1000,
+    },
+  ]);
 
-    handleChange = (e) => {
-        this.setState({
-            value: e.target.value,
-        });
-    };
+  // state = {
+  //     value: "",
+  //     items:
+  // };
 
-    handleAdd = () => {
-        this.setState({
-            items: [{
-                    title: this.state.value,
-                    id: Math.random() * 20000,
-                },
-                ...this.state.items,
-            ],
-            value: ''
-        });
-    };
-    handleDelete = (id) => {
-        console.log(id);
-        console.log("test");
-        this.setState({
-            items: this.state.items.filter((item) => item.id !== id),
-        });
-    };
-    render() {
-        return ( <
-            div className = { "inner-container" } >
-            <
-            h1 > To Do List App < /h1> <
-            InputBox value = { this.state.value }
-            handleChange = { this.handleChange }
-            handleAdd = { this.handleAdd }
-            /> <
-            List items = { this.state.items }
-            handleDelete = { this.handleDelete }
-            /> < /
-            div >
-        );
-    }
-}
+  handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-export default Home;
+  handleAdd = () => {
+    setListItems([
+      ...listItems,
+      {
+        title: value,
+        id: Math.random() * 20000,
+      },
+    ]);
+
+    setValue('');
+
+    // this.setState({
+    //     items: [
+    //         {
+    //             title: this.state.value,
+    //             id: Math.random() * 20000,
+    //         },
+    //         ...this.state.items,
+    //     ],
+    //     value: ''
+    // });
+  };
+  handleDelete = (id) => {
+    setListItems(listItems.filter((item) => item.id !== id));
+    // this.setState({
+    //     items: this.state.items.filter((item) => item.id !== id),
+    // });
+  };
+  return (
+    <div className={'inner-container'}>
+      <h1> To Do List App </h1>
+      <InputBox
+        value={value}
+        handleChange={handleChange}
+        handleAdd={handleAdd}
+      />
+      <List items={listItems} handleDelete={handleDelete} />
+    </div>
+  );
+};
